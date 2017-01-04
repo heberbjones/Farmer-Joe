c487833727809205f2aaaf31e004a64dce25f04d
heberbjones <heberbjones@gmail.com>
2017-01-04 09:10:17 -0700

    More organized
    This keeps the JS more organized.

--- farmerjoe/home/home.js
+++ farmerjoe/home/home.js
@@ -1,3 +1,32 @@
-var joespostop = 417;
+ function getkey (event) {
+ key = event.keyCode;
+
+ if (key == 16 && joesposleft >= 800 && joespostop >= 300 && joesposleft <= 934 && joespostop <= 550) {
+	 window.location = "backyard.html";
+ }
+ 
+ //from here to the next *, is used to keep Joe inside the game area.
+ 
+  if (key == 39) {
+	 joesposleft += joesSpd;
+	 }
+
+  if (key == 37) {
+	 joesposleft -= joesSpd;
+ }
+ 
+ if (key == 38) {
+	 joespostop -= joesSpd;
+ }
+ if (key == 40) {
+	 joespostop += joesSpd;
+ }
+ 
+ 
+placeJoe();
+
+//*
+
+}var joespostop = 417;
 var joesposleft = 900;
 
